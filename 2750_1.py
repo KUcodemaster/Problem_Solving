9d014f94847abc54e37d6e48b5efa7ab33460984
Jaehoon Shin <78628241+KUcodemaster@users.noreply.github.com>
2023-02-15 21:32:22 +0900

    Update 2750_1.py

--- 2750_1.py
+++ 2750_1.py
@@ -8,11 +8,11 @@ for i in range(n):
 
 
 for i in range(n-1):
-	for j in range(i+1, n):
-		if arr[i] > arr[j]:
-			temp = arr[j]
-			arr[j] = arr[i]
-			arr[i] = temp
+	for j in range(n-i-1):
+		if arr[j] > arr[j+1]:
+			temp = arr[j+1]
+			arr[j+1] = arr[j]
+			arr[j] = temp
 
 for i in arr:
 	print(i)

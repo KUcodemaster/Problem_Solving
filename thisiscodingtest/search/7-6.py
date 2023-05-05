9d89e252ee7f6f30184a20e9b0bb2b59ebdcdf94
KUcodemaster <jh9485@korea.ac.kr>
2023-05-05 10:27:35 +0900

    DP

--- thisiscodingtest/search/7-6.py
+++ thisiscodingtest/search/7-6.py
@@ -4,19 +4,21 @@ data = list(map(int, input().split()))
 
 data.sort()
 
+# 이진 탐색을 위한 시작점과 끝점 설정
 len_max = data[-1]
 len_min = 0
+
 res = 0
 while len_min <= len_max:
     mid = (len_min+len_max) // 2
-
     ans = 0
+
     for i in range(n):
         if data[i] > mid:
             ans += data[i] - mid
     
     if ans >= m:
-        res = max(mid, res)
+        res = mid
         len_min = mid + 1
     else:
         len_max = mid - 1
